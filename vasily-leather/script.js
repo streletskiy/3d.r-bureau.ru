3abaa865c9bf0d3b45f52b8f26f1fa37e67e774e
Bogdan Strielecki <nortondeicide@gmail.com>
2025-04-09 09:56:01 +0300

    Upload files

---
+++ vasily-leather/script.js
@@ -0,0 +1,21 @@
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+    event.target.removeEventListener('progress', onProgress);
+  } else {
+    progressBar.classList.remove('hide');
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+const modelViewerColor = document.querySelector('model-viewer');
+
+document.querySelector('#color-controls').addEventListener('click', (event) => {
+	const colorString = event.target.dataset.color;
+	const material = modelViewerColor.model.materials[1];
+	material.pbrMetallicRoughness.setBaseColorFactor(colorString);
+});
